a288d6522d92bbd8bc5793dc2a21c9614766999f
jefferson-bit <jhonsonbabi@hotmail.com>
2022-01-12 08:01:21 -0300

    configuração inicial do projeto

---
+++ src/main/java/com/crash/sqs/TesteSqsApplication.java
@@ -0,0 +1,13 @@
+package com.crash.sqs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteSqsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteSqsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crash/sqs/TesteSqsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crash.sqs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteSqsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
